931aa4e7d36af0cfd1d03b26cf8355ff348b4c9f
TrabalhoPoo2023 <trabalhopoo.2023@gmail.com>
2023-10-29 17:22:54 -0400

    .

--- Menu.java
+++ Menu.java
@@ -38,46 +38,13 @@ public class Menu {
 
 
  private void Professor(){
-    ProfessorServico serv = new ProfessorServico();
-    for (Professor professor : serv.Listar()) {
-        System.out.println("-----------------------------------");
-        professor.Imprimir();
-    }
-    private void ExibirmenuProfessor(){
-        int continuar = 99;
-        do
-        {
-        System.out.println("\n\tControle Academicon\n");
-        System.out.println("1. Professor\n");
-        System.out.println("2. Aluno\n");
-        System.out.println("0. Sair\n");
-        continuar = this.scan.nextInt();
- 
-        switch(continuar){
-        case 1:
-        this.Professor();
-        break;
-        case 2:
-        this.Aluno();
-        break;
-        case 3:
-        this.Sair();
-        break;
-        default:
-        System.out.println("Digite uma opção válida\n");
-    }
-    } while(continuar != 0);
  }
- }
-
-
 
  private void Aluno(){
 
  }
 
- private void Sair()
- {
+ private void Sair(){
  System.out.println("Até a próxima...\n");
  }
 }
